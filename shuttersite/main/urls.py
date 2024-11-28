a0726815e301e0888c8bfb91d83637d7b1b60960
Madhav0Seth <madhavseth0@gmail.com>
2024-11-29 00:08:31 +0530

    Add basic login and logout things

--- shuttersite/main/urls.py
+++ shuttersite/main/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 urlpatterns = [
-    path("singedin",views.index,name='home' ),
-    path("",views.login,name='login' ),
+    path("",views.index,name='home'),
+    path("login",views.login,name='login' ),
 ]
--- shuttersite/main/views.py
+++ shuttersite/main/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect(login)
     return render(request ,'index.html')
+
 def login(request):
-    return render(request , "login.html")
+    if request.method == "POST":
+        return render(request , "login.html")
+    return render(request, "login.html")
